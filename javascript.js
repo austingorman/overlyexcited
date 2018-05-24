a77c01ea97ac5540b0e8085406ffbe7cc6ed0a3b
Austin Gorman <austin.gorman@gmail.com>
2018-05-24 09:50:06 -0500

    completed the project

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 // Create an array that contains the words in the sentence
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+let betterSentence = ['I', 'gave', 'hugo', 'a', 'treat', 'and', 'he', 'spit', 'it', 'out', 'because', 'it', 'had', 'a', 'pill', 'in', 'it'];
 
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
-*/
+    */
+   // Each time the for loop executes, you're going to add one more word to this string
 function addExcitement(theWordArray, symbol) {
-
-    // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = "";
     let exCount = "";
     for (let i = 0; i < theWordArray.length; i++) {
@@ -24,7 +24,7 @@ function addExcitement(theWordArray, symbol) {
 
 }
 // Invoke the function and pass in the array
-addExcitement(sentence, "!")
+addExcitement(betterSentence, "!")
 
 
 
